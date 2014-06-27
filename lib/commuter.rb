36e574474ad33b39f8067b75ee32b0d55c409223
Peggy Griffin <seishingithub@gmail.com>
2014-06-27 16:30:53 -0600

    Sorted by unique commuters

--- lib/commuter.rb
+++ lib/commuter.rb
@@ -8,16 +8,28 @@ class Commuter
 
     # set rest of data as value
     @input_data.each do |person|
-      hash[person[0]] = [
-        {
-          week: person[1],
-          day: person[2],
-          mode: person[3],
-          inbound: person[4],
-          outbound: person[5],
-          distance: person[6]
-        }
-      ]
+      if hash.has_key?(person[0])
+        hash[person[0]] <<
+          {
+            week: person[1],
+            day: person[2],
+            mode: person[3],
+            inbound: person[4],
+            outbound: person[5],
+            distance: person[6]
+          }
+      else
+        hash[person[0]] = [
+          {
+            week: person[1],
+            day: person[2],
+            mode: person[3],
+            inbound: person[4],
+            outbound: person[5],
+            distance: person[6]
+          }
+        ]
+      end
     end
     hash
   end
--- spec/commuter_spec.rb
+++ spec/commuter_spec.rb
@@ -1,10 +1,11 @@
 require 'commuter'
 
 describe Commuter do
-  it 'sorts stats by name of commuter' do
+  it 'sorts stats by name of unique commuter' do
     input = [
       ["Emily",4,"Monday","Walk",12,15,0.65],
       ["Gerard",1,"Wednesday","Drive",14,12,5],
+      ["Gerard",2,"Friday","Drive",14,12,5],
       ["Elsa",1,"Thursday","Drive",28,60,24]
     ]
 
@@ -22,6 +23,14 @@ expected = {
     {
       week: 1,
       day: "Wednesday",
+      mode: "Drive",
+      inbound: 14,
+      outbound: 12,
+      distance: 5
+    },
+    {
+      week: 2,
+      day: "Friday",
       mode: "Drive",
       inbound: 14,
       outbound: 12,
